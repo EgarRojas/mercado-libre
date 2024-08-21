66280c08c931a8654d0820159f4f1ee0831d868e
Egar Rojas Martinez <erojasm@bancoripley.com>
2024-08-21 13:48:18 -0400

    dev vista search + css

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,6 +1,8 @@
-// src/components/SearchBox.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './SearchBox.css';
+import logo from '../assets/logo_v2.png';
+import { FaSearch } from 'react-icons/fa'; // Import the search icon from React Icons
 
 const SearchBox = () => {
   const [query, setQuery] = useState('');
@@ -12,15 +14,24 @@ const SearchBox = () => {
   };
 
   return (
-    <form onSubmit={handleSearch}>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Nunca dejes de buscar"
-      />
-      <button type="submit">Buscar</button>
-    </form>
+    <header className="search-header">
+      <div className="logo">
+        <a href="/">
+          <img src={logo} alt="Logo" />
+        </a>
+      </div>
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Nunca dejes de buscar"
+        />
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+    </header>
   );
 };
 
